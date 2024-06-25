8df676a41e2e24778c44621cbb16f5fac8a2ee8c
surya-codeclouds <surya.karmakar@codeclouds.co.in>
2024-06-25 18:41:23 +0530

    FunctionalComponents

---
+++ src/components/FunctionalComponents/styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Styles;
