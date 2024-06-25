fba9d3190099cc561cbfaf8c7d19ba4e2f25265a
surya-codeclouds <surya.karmakar@codeclouds.co.in>
2024-06-25 17:09:33 +0530

    appjs

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>React Native App</Text>
+    </View>
+  );
+};
+
+export default App;
