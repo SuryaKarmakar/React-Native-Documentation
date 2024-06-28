85fd5c1d9c742936b2b386e18ed156fcfa4c8ada
surya-codeclouds <surya.karmakar@codeclouds.co.in>
2024-06-28 19:42:23 +0530

    project cleanup

---
+++ src/resource/routes.js
@@ -0,0 +1,15 @@
+import HomeScreen from '../HomeScreen';
+import ViewRN from '../components/ViewRN';
+
+const Routes = [
+  {
+    name: 'React Native',
+    component: HomeScreen,
+  },
+  {
+    name: 'View',
+    component: ViewRN,
+  },
+];
+
+export default Routes;
