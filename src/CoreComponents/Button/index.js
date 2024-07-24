910ea7bcc522f91d34d585ed9c4b0339cc251e1c
surya-codeclouds <surya.karmakar@codeclouds.co.in>
2024-07-24 16:24:39 +0530

    core component added

---
+++ src/CoreComponents/Button/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Button} from 'react-native';
+
+const ButtonCC = () => {
+  return <></>;
+};
+
+export default ButtonCC;
---
+++ src/CoreComponents/Image/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Image} from 'react-native';
+
+const ImageCC = () => {
+  return <></>;
+};
+
+export default ImageCC;
