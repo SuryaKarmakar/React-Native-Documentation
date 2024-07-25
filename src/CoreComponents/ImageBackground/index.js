aba34b8a259cd8171253d3706df45b23e5a7cd84
surya-codeclouds <surya.karmakar@codeclouds.co.in>
2024-07-25 13:24:35 +0530

    ImageBackground

--- src/CoreComponents/ImageBackground/index.js
+++ src/CoreComponents/ImageBackground/index.js
@@ -1,8 +1,41 @@
 import React from 'react';
-import {ImageBackground} from 'react-native';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
 
 const ImageBackgroundCC = () => {
-  return <></>;
+  const image = {uri: 'https://legacy.reactjs.org/logo-og.png'};
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={image}
+        style={styles.image}
+        imageStyle={styles.imageStyle}>
+        <Text style={styles.text}>Inside</Text>
+      </ImageBackground>
+    </View>
+  );
 };
 
 export default ImageBackgroundCC;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 0.6,
+    justifyContent: 'center',
+    resizeMode: 'cover',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
+  imageStyle: {
+    borderRadius: 20,
+  },
+});
