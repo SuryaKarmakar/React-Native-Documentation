5566dc93c77435cbcf3564b419729174e5571f96
surya-codeclouds <surya.karmakar@codeclouds.co.in>
2024-07-25 13:44:59 +0530

    Image

--- src/CoreComponents/Image/index.js
+++ src/CoreComponents/Image/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, StyleSheet} from 'react-native';
 
 const ImageCC = () => {
-  return <></>;
+  return (
+    <>
+      <Image
+        style={styles.image}
+        source={{
+          uri: 'https://legacy.reactjs.org/logo-og.png',
+        }}
+      />
+    </>
+  );
 };
 
 export default ImageCC;
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
